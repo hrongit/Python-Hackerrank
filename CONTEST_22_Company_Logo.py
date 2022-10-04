04e7405c9388f384af7474307302bdb9563cdd11
HRITIK RAJ <97436607+hrongit@users.noreply.github.com>
2022-10-04 23:54:04 +0530

    Contest 22 Python Hackerrank

---
+++ CONTEST_22_Company_Logo.py
@@ -0,0 +1,14 @@
+#Ques link - https://www.hackerrank.com/contests/int213-contest22-1664892677/challenges/most-commons
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+class OrderedCounter(Counter):
+    pass
+
+if __name__ == '__main__':
+    [print(*c) for c in OrderedCounter(sorted(input())).most_common(3)]
