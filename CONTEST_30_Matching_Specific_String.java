9b6405f09624b616d1bdf483bbbf4d6160ceeb18
HRITIK RAJ <97436607+hrongit@users.noreply.github.com>
2022-10-18 00:06:28 +0530

    Hackerrank Python Contest 30

--- CONTEST_30_Matching_Specific_String.java
+++ CONTEST_30_Matching_Specific_String.java
@@ -1,4 +1,4 @@
-
+//Ques Link - https://www.hackerrank.com/contests/int213-contest30/challenges/matching-specific-string/problem
 
 
 
---
+++ CONTEST_30_Matching_Specific_String.py
@@ -0,0 +1,4 @@
+#Ques Link - https://www.hackerrank.com/contests/int213-contest30/challenges/matching-specific-string/problem
+
+
+Regex_Pattern = r'hackerrank+'    # Do not delete 'r'
