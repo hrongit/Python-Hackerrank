c243212ea6438920e23e50b73233a728eda40fb2
HRITIK RAJ <97436607+hrongit@users.noreply.github.com>
2022-10-21 00:50:50 +0530

    Python Hackerrank contest 33

---
+++ CONTEST33_Valid_PAN_format.py
@@ -0,0 +1,20 @@
+#https://www.hackerrank.com/contests/k21qt-contest33/challenges/valid-pan-format
+
+
+
+
+import re
+
+
+def validate_pan_numbers(numbers):
+  
+    pattern = re.compile('^[A-Z]{5}\d{4}[A-Z]$')
+
+    for number in numbers:
+        print('YES' if pattern.match(number) else 'NO')
+
+
+if __name__ == '__main__':
+    number_count = int(input())
+    numbers = (input() for _ in range(number_count))
+    validate_pan_numbers(numbers)
